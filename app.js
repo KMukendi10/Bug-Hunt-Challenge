32e47d3d87658553e687d5c4686c554ade454fe2
KMukendi10 <125656620+KMukendi10@users.noreply.github.com>
2026-06-04 11:50:58 +0200

    Bug fix: Renamed id to task to avoid variable naming conflicts

--- app.js
+++ app.js
@@ -39,15 +39,15 @@ function addTask(text) {
 
 
 function toggleTask(id) {
-  tasks = tasks.map(function (id) {
-    if (id.id === id) {
-      return { ...id, completed: !id.completed };
+  tasks = tasks.map(function (task) {
+    if (task.id === id) {
+      return { ...task, completed: !task.completed };
     }
-    return id;
+    return task;
   });
 
   renderTasks();
-}
+} // Kazadi - Renamed to task for clearer comparison with the task ID
 
 
 function deleteTask(id) {
